1e274b8ac4b270bba413fea4c63eda531f6124d7
Josh Guffin <josh.guffin@gmail.com>
2011-09-26 13:02:42 -0400

    options screen updates

--- functions.js
+++ functions.js
@@ -2,17 +2,15 @@
 function invalidateCurrentTimer()
 {
     // DEBUG:
-    console.log("Clearing old timer: " + localStorage['interval-id']);
-    if (localStorage['interval-id'] !== '') {
-        clearInterval(localStorage['interval-id']);
-        localStorage['interval-id'] = '';
+    console.log("Clearing old timer: " + localStorage['timeout-id']);
+    if (localStorage['timeout-id'] !== '') {
+        clearTimeout(localStorage['timeout-id']);
+        localStorage['timeout-id'] = '';
     }
 }
 
 /*
  * Update the game count by polling
- * IMPORTANT: DO NOT CALL update() FROM ANYWHERE BUT background.html
- * you will start _many_ timers 
  */
 function update() {
     chrome.browserAction.setBadgeBackgroundColor({color:[255, 0, 0, 0]});
@@ -36,21 +34,19 @@ function update() {
             invalidateCurrentTimer();
 
         } else {
-           
-            // Check if we need to start a timer
-            if (localStorage['interval-id'] === '') {
 
-                // wait for at least 10 seconds and reload the function
-                // TODO: change min => max, 1000 => 10000
-                var wait = Math.min(1000, localStorage['interval']);
-                localStorage['interval-id'] = setInterval(update, wait);
+            // wait for at least 30 seconds and reload the function
+            var wait = Math.max(30000, localStorage['interval']);
 
-                // DEBUG:
-                console.log("Started new timer " + localStorage['interval-id'] + ", waiting "+ (wait / 1000) + " seconds");
-            }
+            localStorage['timeout-id'] = setTimeout(update, wait);
+
+            // DEBUG:
+            //console.log("Started new timer " + localStorage['timeout-id'] + ", waiting "+ (wait / 1000) + " seconds");
 
             localStorage['logged-in']   = 'true';
         }
+        // DEBUG:
+        console.log("Timer " + localStorage['timeout-id'] + " reported \""+ response + "\" ");
 
         // update the numerical overlay
         chrome.browserAction.setBadgeText({text:response});
